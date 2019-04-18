081459f1efb404cd88db5dcdbcca99b2fe075252
William Spicer (BSL) <williams2@kpit.com>
2019-04-18 17:37:16 -0400

    starting a new project with the MERN stack

---
+++ MERN/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+    'mongodb+srv://Orin:orin@orinscluster-mlzlz.mongodb.net/test?retryWrites=true'
+};
---
+++ MERN/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to mongoDB through mongoose
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB connected!'))
+    .catch(err => console.log(err));
+
+app.get('/',(req,res) => {
+    res.send('Hello!');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,() => console.log(`Server running on ${port}`));
