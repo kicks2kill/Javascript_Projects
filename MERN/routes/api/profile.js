58af176b538ee0cbe56b8f25d53b2df0e62dde9e
William Spicer (BSL) <williams2@kpit.com>
2019-04-21 16:48:03 -0400

    added a handle and user_id validation in the routes/api/profile.js

--- MERN/routes/api/profile.js
+++ MERN/routes/api/profile.js
@@ -23,6 +23,7 @@ router.get('/',passport.authenticate('jwt',{  session: false  }), (req,res) => {
     const errors = {};
 
     Profile.findOne({ user: req.user.id })
+    .populate('user',['name', 'avatar'])
     .then(profile => {
         if(!profile) {
             errors.noprofile = "There is no profile for this user";
@@ -32,6 +33,48 @@ router.get('/',passport.authenticate('jwt',{  session: false  }), (req,res) => {
     })
     .catch(err => res.status(404).json(err));
 });
+
+
+//@route GET api/profile/handle/:handle
+//@desc     Get profile by handle
+//@access   public
+
+router.get('/handle/:handle', (req,res) => {
+    const errors = {};
+
+    Profile.findOne({  handle: req.params.handle })
+    .populate('user',['name','avatar'])
+    .then(profile => {
+        if(!profile){
+            errors.noprofile = 'There is no profile for this user';
+            res.status(404).json(errors);
+        }
+        res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+
+
+//@route GET api/profile/user/:user_id
+//@desc     Get profile by user ID
+//@access   public
+
+router.get('/user/:user_id', (req,res) => {
+    const errors = {};
+    
+    Profile.findOne({  handle: req.params.user_id })
+    .populate('user',['name','avatar'])
+    .then(profile => {
+        if(!profile){
+            errors.noprofile = 'There is no profile for this user';
+            res.status(404).json(errors);
+        }
+        res.json(profile); 
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 
 
 //@route POST api/profile
