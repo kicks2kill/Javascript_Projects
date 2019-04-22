8be08757c1fb746fd16a151b586f471903c2102b
William Spicer (BSL) <williams2@kpit.com>
2019-04-22 10:17:22 -0400

    added the ability to remove likes from posts

--- MERN/routes/api/posts.js
+++ MERN/routes/api/posts.js
@@ -102,6 +102,35 @@ router.post('/like/:id',passport.authenticate('jwt',{session: false}, (req,res) 
         })
         .catch(err => res.status(404).json({postnotfound: 'no post found'}));
     })
-}))
+}));
 
+
+//@route POST api/posts/unlike/:id
+//@desc     Like post id
+//@access   private
+
+router.post('/unlike/:id',passport.authenticate('jwt',{session: false}, (req,res) => {
+    Profile.findOne({ user: req.user.id  })
+    .then(profile => {
+        Post.findById(req.params.id)
+        .then(post => {
+            if(post.likes.filter(like => like.user.toString() === req.user.id).length == 0) {
+                return res.status(400).json({ notliked: 'You have not yet liked this post'  });
+            }
+
+            //get remove index
+           const removeIndex = post.likes
+           .map(item => item.user.toString())
+           .indexOf(req.user.id);
+
+           //splice out of array
+
+           post.likes.splice(removeIndex, 1);
+
+           //Save 
+           post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({postnotfound: 'no post found'}));
+    })
+}));
 module.exports = router;
