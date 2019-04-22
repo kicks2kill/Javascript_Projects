960827b5b5cd8a129f6d15da03a23abebb06275b
William Spicer (BSL) <williams2@kpit.com>
2019-04-22 11:36:58 -0400

    added ability to remove/add comments

--- MERN/routes/api/posts.js
+++ MERN/routes/api/posts.js
@@ -32,7 +32,7 @@ router.get('/', (req,res) => {
 //@route GET api/posts/posts/:id
 //@desc     Get posts by id
 //@access   public 
-
+ 
 router.get('/:id', (req,res) => {
     Post.findById(req.params.id)
     .then(post => res.json(post))
@@ -133,4 +133,66 @@ router.post('/unlike/:id',passport.authenticate('jwt',{session: false}, (req,res
         .catch(err => res.status(404).json({postnotfound: 'no post found'}));
     })
 }));
+
+
+//@route POST api/posts/comment/:id
+//@desc     Add a comment to post
+//@access   private
+
+router.post('/comment/:id', passport.authenticate('jwt', {session: false}, (res,res) => {
+    
+    const {errors, isValid } = validatePostInput(req.body);
+    
+    //check validation
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    
+    Post.findById(req.params.id)
+    .then(post => {
+        const newComment = {
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        }
+
+        //Add to commments array
+        post.comments.unshift(newComment);
+
+        //save
+        post.save().then(post => res.json(post))
+        .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+    })
+}));
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//@desc     Remove a comment from post
+//@access   private
+
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', {session: false}, (res,res) => {
+    Post.findById(req.params.id)
+    .then(post => {
+        //Check to see if comment exists
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+            return res.status(404).json({commentnotexists: 'Comment does not exist'});
+        }
+
+        //get remove index
+        const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id);
+
+
+        //splice comment out of array
+        post.comments.splice(removeIndex,1);
+
+        post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+}));
+
+
+
+
 module.exports = router;
