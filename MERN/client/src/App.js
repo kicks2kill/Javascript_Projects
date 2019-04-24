cb2cc4c5cc7ef230144d574a5dd0608fc4ee6ca8
William Spicer (BSL) <williams2@kpit.com>
2019-04-24 09:16:15 -0400

    added componentWillReceiveProps(nextProps) to client/src/components/Login.js and applied classnames wrapper to the classNames(form-control). Also imported jwt_decode into client/src/app.js and implemented checking for the auth token header, decoded the token and set user to isAuthenicated

--- MERN/client/src/App.js
+++ MERN/client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {  BrowserRouter as Router, Route } from 'react-router-dom'; 
 import { Provider } from 'react-redux';
 import store from './store';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';  
@@ -10,6 +13,17 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+
+
+//Check for token
+if(localStorage.jwtToken) {
+  //Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //Decode token and get user info/expiration
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //Set user and isAuthenicated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 const store = createStore(() => [], {}, applyMiddleware());
 
--- MERN/client/src/components/auth/Login.js
+++ MERN/client/src/components/auth/Login.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../actions/authActions';
+
 
  class Login extends Component {
     constructor() {
@@ -9,6 +14,15 @@ import React, { Component } from 'react'
             errors: {}
         };
     }
+    componentWillReceiveProps = (nextProps) => {
+      if(nextProps.auth.isAuthenicated) {
+        this.props.history.push('/dashboard');
+      }
+
+      if(nextProps.errors) {
+        this.setState({errors: nextProps.errors});
+      }
+    }
 
     onChange = (e) => {
         this.setState({[e.target.name]: e.target.value});
@@ -16,14 +30,15 @@ import React, { Component } from 'react'
 
     onSubmit = (e) =>  {
         e.preventDefault();
-        const user = {
+        const userData = {
             email: this.state.email,
             password: this.state.password,
         }
-        console.log(user);
+        this.props.loginUser(userData);
     }
 
   render() {
+    const { errors } = this.state;
     return (
       <div>
         <div className="login">
@@ -34,10 +49,12 @@ import React, { Component } from 'react'
                 <p className="lead text-center">Sign in to your DevConnector account</p>
                 <form onSubmit = {this.onSubmit}>
                     <div className="form-group">
-                    <input type="email" className="form-control form-control-lg" placeholder="Email Address" name="email"  value={this.state.email} onChange={this.onChange}/>
+                    <input type="email" className={classnames("form-control form-control-lg", { 'is-invalid': errors.email})} placeholder="Email Address" name="email"  value={this.state.email} onChange={this.onChange}/>
+                    {errors.email && (<div className="invalid-feedback"> {errors.email} </div>)}
                     </div>
                     <div className="form-group">
-                    <input type="password" className="form-control form-control-lg" placeholder="Password" name="password"  value={this.state.password} onChange={this.onChange}/>
+                    <input type="password" className={classnames("form-control form-control-lg", { 'is-invalid': errors.password})} placeholder="Password" name="password"  value={this.state.password} onChange={this.onChange}/>
+                    {errors.password && (<div className="invalid-feedback"> {errors.password} </div>)}
                     </div>
                     <input type="submit" className="btn btn-info btn-block mt-4" />
                 </form>
@@ -49,5 +66,15 @@ import React, { Component } from 'react'
     )
   }
 }
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
 
-export default Login;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
