02055cd590746998f1c5bc8d4b61873f38304119
Orin <w.spicerx@gmail.com>
2019-05-21 09:03:38 -0400

    Creating a vue project that allows a user to enter a maximum of 10 quotes and view those in a grid on a webpage

---
+++ Vue_quotes/quotes/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  export default {
+    data: function() {
+      return {
+        quotes: [],
+        maxQuotes: 10
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid;
+    }
+  }
+</script>
+
+<style>
+
+</style>
