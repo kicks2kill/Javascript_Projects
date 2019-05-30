a3bbabacfc5ef612caf98bdf9368a1e3e6d18474
Orin <w.spicerx@gmail.com>
2019-05-30 15:44:04 -0400

    added a new project called Vue_quotes

--- Vue_quotes/quotes/src/App.vue
+++ Vue_quotes/quotes/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="container">
-    <app-quote-grid :quotes="quotes"></app-quote-grid>
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue';
   export default {
     data: function() {
       return {
@@ -14,7 +25,20 @@
       }
     },
     components: {
-      appQuoteGrid: QuoteGrid;
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
+    },
+    methods: {
+      newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes) {
+          return alert('Please delete quote first');
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index,1);
+      }
     }
   }
 </script>
