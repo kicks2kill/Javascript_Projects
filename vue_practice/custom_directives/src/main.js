7e3ffa1bc8a278bf326ef11363cfc231c2e0b704
Spicer <william.spicer@teleflex.com>
2019-07-02 14:32:03 -0400

    added a validation check on binding.modifiers[] to see if the modifier 'delayed' is present, created a variable delay and wrapped the previous binding.arg =='background' if block in a setTimeout method as a callback

--- vue_practice/custom_directives/src/main.js
+++ vue_practice/custom_directives/src/main.js
@@ -7,11 +7,16 @@ Vue.directive('highlight', {
   bind(el,binding,vnode) {
     //el.style.backgroundColor = 'green';
     //el.style.backgroundColor = binding.value;
-    if(binding.arg == 'background') {
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    if(binding.modifiers['delayed']) {
+      delay = 3000;
     }
+    setTimeout(() => {
+      if(binding.arg == 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    },delay);
   }
 });
 
