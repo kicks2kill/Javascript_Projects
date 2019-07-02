e5ddc851f7076e5f14f33a0641a152461ef48fbd
Spicer <william.spicer@teleflex.com>
2019-07-02 14:44:27 -0400

    introduced a new modifier called 'blink' with variables mainColor,secondColor and currentColor. Introduced a setInterval callback inside of the initial setTimeout() callback where it checks if the binding.modifiers['blink'] is true

--- vue_practice/custom_directives/src/App.vue
+++ vue_practice/custom_directives/src/App.vue
@@ -12,7 +12,7 @@
        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
          <h1>Custom Directives</h1>
          <p v-highlight:background.delayed="'red'">Color this</p>
-        <p v-local-highlight:background.delayed="'red'">Color this</p>
+        <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
       </div>
     </div>
   </div>
@@ -30,16 +30,33 @@ export default {
     if(binding.modifiers['delayed']) {
       delay = 3000;
     }
-    setTimeout(() => {
-      if(binding.arg == 'background') {
-        el.style.backgroundColor = binding.value;
+    if(binding.modifiers['blink']){
+      var mainColor = binding.value;
+      var secondColor = 'blue';
+      let currentColor = mainColor;
+      setTimeout(() => {
+        setInterval(() => {
+        currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor;
+        if(binding.arg == 'background') {
+          el.style.backgroundColor = currentColor;
       } else {
-        el.style.color = binding.value;
+          el.style.color = currentColor;
+        }
+        },3000)
+        
+      },delay);
+    } else {
+      setTimeout(() => {
+        if(binding.arg == 'background') {
+          el.style.backgroundColor = binding.value;
+      } else {
+          el.style.color = binding.value;
         }
       },delay);
-     }
     }
   }
+}
+}
 }
 </script>
 
