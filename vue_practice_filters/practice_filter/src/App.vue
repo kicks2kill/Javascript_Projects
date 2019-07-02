d975005edf5efad560e1f7c7d5329c0c855627d3
Spicer <william.spicer@teleflex.com>
2019-07-02 15:37:04 -0400

    added a global and local filter... going to work on mixins next

--- vue_practice_filters/practice_filter/src/App.vue
+++ vue_practice_filters/practice_filter/src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <div id="app">
-   
+  <div id="container">
+    <div id="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters and Mixins</h1>
+        <p>{{ text | toUppercase}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,7 +13,16 @@
 
 
 export default {
-
+  data() {
+    return {
+      text: 'Hello There!'
+    }
+  },
+  filters: {
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  }
 }
 </script>
 
