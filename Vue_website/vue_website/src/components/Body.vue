df4e0821f2f126e1f99155551fa028e6fe2a63b9
Orinxlm <w.spicerx@gmail.com>
2020-06-10 10:02:26 -0400

    Not near complete - still trying to decide if I like the layout or not.

--- Vue_website/vue_website/src/components/Body.vue
+++ Vue_website/vue_website/src/components/Body.vue
@@ -22,6 +22,23 @@
                  <p><em>Figma</em></p>
             </div>
         </div>
+        <div class="row">
+            <img src="../assets/myself.jpg" style="width:18em; height: 18em; border-radius: 10px;" alt="William Spicer"/>
+        </div>
+        <div class="row">
+            <div style="width:100%;">
+                <p class="subject-paragraph"><em>Projects</em></p>
+                <img src="../assets/playstation.png" style="width: 10em; height: 10em; margin-right: 5em; margin-left: 2em; border: 1px solid white; border-radius: 5px;"/>
+                <img src="../assets/team.svg" style="width: 10em; height: 10em; border: 1px solid white; border-radius: 5px;"/>
+                <p style="opacity: 70%; font-size: 18px;">Click for more information</p> <!-- work on this -->
+            </div>
+        </div>
+        <div class="row">
+        <div style="width:100%;">
+                <p class="subject-paragraph">Design Thinking & Problem Solving</p>
+                <span class="sub-text">I believe in the power of research. I enjoy challenging myself by learning new material and the real world applications associated with the material. The problem solving process is something that I respect and admire as I strive to condition myself to surmise on difficult decisions.</span>
+            </div>
+        </div>
     </div>  
 </template>
 <script>
@@ -41,13 +58,14 @@ export default {
     display:inline-flex;
     flex-direction: row;
     justify-content: center;
-    
 }
 
 .row div {
     background-color:rgba(0,0,0,.8);
     color: ivory;
     padding: 1em;
+    
+    
 }
 
 div p {
@@ -63,6 +81,19 @@ div img {
 
 }
 
+.subject-paragraph {
+    font-size: 26px; text-decoration: underline; 
+    text-decoration-color:#90ddf0;
+    text-underline-position: under right;
+    text-align: center;
+}
 
+.sub-text {
+    font-size: 15px;
+    color: ivory;
+    margin: 10px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+}
 
 </style>
