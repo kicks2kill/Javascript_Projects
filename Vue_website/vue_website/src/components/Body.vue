b02b40216ab0eb4da348f8c9ff23cc2bb43bd278
Orinxlm <w.spicerx@gmail.com>
2020-06-10 11:51:57 -0400

    adjusting some of the positioning

--- Vue_website/vue_website/src/components/Body.vue
+++ Vue_website/vue_website/src/components/Body.vue
@@ -6,8 +6,7 @@
                 <p><em>CSS3</em></p>
             </div>
             <div>
-                <img src="../assets/javascript.png"/>
-                <p><em>Javascript</em></p>
+               
             </div>
             <div>
                 <img src="../assets/vue.png"/>
@@ -26,15 +25,25 @@
         <div class="row">
             <div style="width:100%;">
                 <p class="subject-paragraph"><em>Projects</em></p>
+                 <p style="opacity: 70%; font-size: 18px;">(Click for more information)</p> <!-- work on this -->
                 <img src="../assets/playstation.png" style="width: 10em; height: 10em; margin-right: 5em; margin-left: 2em; border: 1px solid white; border-radius: 5px;"/>
                 <img src="../assets/team.svg" style="width: 10em; height: 10em; border: 1px solid white; border-radius: 5px;"/>
-                <p style="opacity: 70%; font-size: 18px;">Click for more information</p> <!-- work on this -->
             </div>
         </div>
         <div class="row">
         <div style="width:100%;">
                 <p class="subject-paragraph">Design Thinking & Problem Solving</p>
                 <span class="sub-text">I believe in the power of research. I enjoy challenging myself by learning new material and the real world applications associated with the material. The problem solving process is something that I respect and admire as I strive to condition myself to surmise on difficult decisions.</span>
+            </div>
+        </div>
+          <div class="row">
+             <div style="width:100%;">
+                <p class="subject-paragraph">About me</p>
+                <span class="sub-text">Hello! I am William Spicer, a front-end engineer with a focus on desirability and usability. Currently, I have 3 years of experience designing and developing websites. I'm a strong believer of continous research and bettering myself as well as helping others do the same. When I'm not working or developing, I enjoy spending time researching into various topics such as; Chemistry, Math, and Art. I am an avid miniature painter and moderate a few painting discords in regards to that subject.</span>
+                <p style="font-size: 16px;"><strong>Find me on:</strong> </p>
+                <a href="https://www.linkedin.com/in/william-spicer/"><img style="width: 5em; height: 5em; border-radius: 10px; " src="../assets/linkedin.png"/></a>
+                <a href="https://github.com/kicks2kill/"><img style="width: 5em; height: 5em; border-radius: 10px;" src="../assets/github.png"/></a>
+                <a href="mailto:w.spicerx@gmail.com"><img style="width: 5em; height: 5em; border-radius: 10px;" src="../assets/gmail.png"/></a>
             </div>
         </div>
     </div>  
@@ -56,18 +65,18 @@ export default {
     display:inline-flex;
     flex-direction: row;
     justify-content: center;
-    margin: 5px;
-    border-radius: 5px;
+    margin: 2px;
 }
 
 .row div {
     background-color:rgba(0,0,0,.8);
     color: ivory;
     padding: 1em;
-    
-    
+    border-radius: 1px;
 }
-
+img + p {
+    margin-left: 0;
+}
 div p {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 20px;
@@ -91,9 +100,13 @@ div img {
 .sub-text {
     font-size: 15px;
     color: ivory;
-    margin: 10px;
+    margin: 5px;
+    left: 0;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
 
+div a {
+    color: white;
 }
 
 </style>
