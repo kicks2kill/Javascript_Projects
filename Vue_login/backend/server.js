223f7ce74818eee87a28351a6ee764f3bb7ddb5a
Orinxlm <w.spicerx@gmail.com>
2020-06-12 13:58:18 -0400

    added bodyparser to help parse the information being passed. Also, right now only an empty json object is being returned when making a post request - plan to fix that

--- Vue_login/backend/server.js
+++ Vue_login/backend/server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
 app.use(cors());
-
+app.use(bodyParser.json());
 
 app.get('/messages', (req, res) =>{
     const messages = ["h", "1", "something else"];
 
     res.send(messages);
+});
+
+app.post('/messages', (req,res) => {
+    console.log(req.body);
+    res.json({});
 });
 
 app.listen(port, () => console.log('App is running'));
