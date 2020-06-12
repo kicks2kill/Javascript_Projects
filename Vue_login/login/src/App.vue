0959495e4dcfce4d7e83eec3866d43fc77bc07ad
Orinxlm <w.spicerx@gmail.com>
2020-06-12 13:11:29 -0400

    creating a new project to work with vuex and vuetify

---
+++ Vue_login/login/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Messaages/>
+  </div>
+</template>
+
+<script>
+import Messages from './components/Messages.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Messages
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue_login/login/src/components/Messages.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="messages">
+    <h4>Mesages</h4>
+    <ul v-for="message in messages">
+      <li>{{message}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: ["h", "1"]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
