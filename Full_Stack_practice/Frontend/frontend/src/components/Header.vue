dd35d1ea6cca62e1910bf5a0031bc898967da0ee
Orinxlm <w.spicerx@gmail.com>
2020-06-24 21:31:33 -0400

    trying to resolve if vue router is currently on the login page, and if so, then present the login view. If not, then display the home page

---
+++ Full_Stack_practice/Frontend/frontend/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="container mx-auto">
+    <div class="flex">
+        <div class="w-3/4 bg-indigo-500 h-12"></div>
+        <div class="w-1/4 bg-indigo-700 h-12"></div>
+    </div>
+</div>
+</template>
