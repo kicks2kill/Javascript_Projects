da3126387d864d3d9c85076ac29a46a6710ce9fe
Orinxlm <w.spicerx@gmail.com>
2020-06-25 22:38:27 -0400

    still adjusting the styling on the home page

--- Full_Stack_practice/Frontend/frontend/src/components/Home.vue
+++ Full_Stack_practice/Frontend/frontend/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container mx-auto">
-      <div class="max-w-sm w-full lg:max-w-full lg:flex my-6">
+      <div class="max-w-sm w-full lg:max-w-full lg:flex my-6 justify-center">
       <div class="border-solid border-2 p-8 border-gray-300 flex flex-col justify-between leading-normal">
           <span>Create a post</span>
           <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-blue-700 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text">
@@ -8,7 +8,7 @@
   </div>
 </div>
     <div class="mt-8"> 
-    <span class="text-red-700 text-lg">Hide Posts</span> 
+    <span class="text-red-700 text-lg ml-2">Hide Posts</span> 
     <div class="float-right mr-56">
       <span>Filter By:</span> 
         <select class="ml-2">
@@ -19,9 +19,10 @@
         </div>
     </div>
     <div class="max-w-sm w-full my-4 lg:max-w-full lg:flex">
-      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+      <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal shadow-md">
         <div class="mb-8">
           <div class="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
+             <span class="text-gray-600 leading-none float-right -mt-8">Total Views: 109</span>
           <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
         </div>
         <div class="flex items-center">
@@ -35,9 +36,10 @@
   </div>
    <div class="mx-auto my-4">
     <div class="max-w-sm w-full lg:max-w-full lg:flex">
-        <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal shadow-md">
           <div class="mb-8">
             <div class="text-gray-900 font-bold text-xl mb-2">Caffeine + Developer = ?</div>
+              <span class="text-gray-600 leading-none float-right -mt-8">Total Views: 253</span>
             <p class="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
           </div>
           <div class="flex items-center">
