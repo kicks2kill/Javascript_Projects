492d696042916c0b142f505d323ad88074f2c54e
Orinxlm <w.spicerx@gmail.com>
2020-07-02 23:07:32 -0400

    Moving more solutions to algorithm exercises into this repo

---
+++ Algorithms/InvertBinaryTree.js
@@ -0,0 +1,21 @@
+function invertBinaryTree(tree) {
+
+	const queue = [tree];
+	while(queue.length) {
+		const current = queue.shift();
+		if(current === null) continue;
+		swapLeftAndRight(current);
+		queue.push(current.left);
+		queue.push(current.right);
+	}
+}
+
+
+function swapLeftAndRight(tree) {
+	const left = tree.left;
+	tree.left = tree.right;
+	tree.right = left;
+}
+
+
+exports.invertBinaryTree = invertBinaryTree;
--- Full_Stack_practice/Frontend/frontend/src/components/Home.vue
+++ Full_Stack_practice/Frontend/frontend/src/components/Home.vue
@@ -145,7 +145,7 @@
 export default {
   data() {
     return {
-      messageBody: " ",
+      messageBody: "",
       isHidden: false,
     }
   },
